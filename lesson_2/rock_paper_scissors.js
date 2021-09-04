525422fadb46e3ac1eff4f9efc2525234f1e1638
trent-cooper <00trent@protonmail.com>
2021-09-03 19:35:34 -0500

    Added all bonus features to Rock Paper Scissors program.

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,42 +1,91 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
-let prompt = (message) => console.log(`=> ${message}`);
+const prompt = (message) => console.log(`=> ${message}`);
 
-let diplayWinner = (playerInput, computerInput) => {
-  if ((playerInput === 'rock' && computerInput === 'scissors') ||
-      (playerInput === 'paper' && computerInput === 'rock') ||
-      (playerInput === 'scissors' && computerInput === 'paper')) {
-    prompt('You win!');
+function returnWinner(playerInput, computerInput) {
+  if ((playerInput === 'rock' && ['scissors', 'lizard'].includes(computerInput)) ||
+    (playerInput === 'paper' && ['rock', 'spock'].includes(computerInput)) ||
+    (playerInput === 'scissors' && ['paper', 'lizard'].includes(computerInput)) ||
+    (playerInput === 'lizard' && ['spock', 'paper'].includes(computerInput)) ||
+    (playerInput === 'spock' && ['scissors', 'rock'].includes(computerInput))) {
+    return 'You win!';
   } else if (playerInput === computerInput) {
-    prompt ('It\'s a tie');
+    return 'It\'s a tie';
   } else {
-    prompt ('Computer wins!');
+    return 'Computer wins!';
   }
-};
+}
 
-while (true) {
+function returnFullSelection(choice) {
+  switch (choice[0]) {
+    case 'r':
+      return 'rock';
+    case 'p':
+      return 'paper';
+    case 'l':
+      return 'lizard';
+    case 's':
+      if (choice[1] === 'c') {
+        return 'scissors';
+      } else if (choice[1] === 'p') {
+        return 'spock';
+      }
+  }
+  return 'invalid';
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+while (playerScore < 3 && computerScore < 3) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = readline.question().toLowerCase();
+  let choiceFull = returnFullSelection(choice);
 
-  while (!VALID_CHOICES.includes(choice)) {
+  while (!VALID_CHOICES.includes(choiceFull)) {
     prompt('That\'s not a valid choice, please try again');
     choice = readline.question().toLowerCase();
+    choiceFull = returnFullSelection(choice);
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  prompt(`You chose ${choice}, Computer chose ${computerChoice}.`);
+  prompt(`You chose ${choiceFull}, Computer chose ${computerChoice}.`);
 
-  diplayWinner(choice, computerChoice);
+  let roundMessage = returnWinner(choiceFull, computerChoice);
+  prompt(roundMessage);
 
-  prompt('Do you want to play again? (Y/N)');
-  let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
+  if (roundMessage[0] === 'Y') {
+    playerScore++;
+  } else if (roundMessage[0] === 'C') {
+    computerScore++;
   }
 
-  if (answer[0] !== 'y') break;
+  prompt(`Current Score | You: ${playerScore} | Computer: ${computerScore}`);
+
 }
+
+if (playerScore === 3) {
+  prompt('You\'re the grand winner! Congrats! Thanks for playing.');
+} else {
+  prompt(`The computer wins :( Better luck next time!`);
+}
+
+// prompt('Do you want to play again? (Y/N)');
+// let answer = readline.question().toLowerCase();
+// while (answer[0] !== 'n' && answer[0] !== 'y') {
+//   prompt('Please enter "y" or "n".');
+//   answer = readline.question().toLowerCase();
+// }
+
+// if (answer[0] !== 'y') break;
+
+
+// if ((playerInput === 'rock' && computerInput === ('scissors' || 'lizard')) ||
+//   (playerInput === 'paper' && computerInput === ('rock' || 'spock')) ||
+//   (playerInput === 'scissors' && computerInput === ('paper' || 'lizard')) ||
+//   (playerInput === 'lizard' && computerInput === ('spock' || 'paper')) ||
+//   (playerInput === 'spock' && computerInput === ('scissors' || 'rock')))
+
